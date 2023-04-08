5d7f0c4eb8ffaefbbc7e3e29fb6c49da92921602
skittels15 <on-game15@ya.ru>
2023-04-08 22:12:23 +0300

    camellia: Banana Official now!

--- device.mk
+++ device.mk
@@ -273,6 +273,10 @@ PRODUCT_PACKAGES += \
     NfcNci \
     Tag
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
